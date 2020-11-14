c47dca9bdc341844d341af5998ca921e4b4457bb
Aiden <aiden.marques94@gmail.com>
2020-11-14 18:27:49 -0500

    commit tree ed1539a653d76f273c05f5fc5c5774b9c878bd02

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,37 @@
 # Write your code here.
+
+
+katz_deli = []
+
+def line(place)
+    
+    if place.length == 0
+        puts "The line is currently empty."
+    else
+        positions = place.map.with_index(1) { |name,i| "#{i}. #{name}"} 
+        puts "The line is currently: " + positions.join(" ")
+    end 
+end 
+
+def take_a_number(person,new_name)
+    if person.length == 0 
+        person << new_name 
+       puts "Welcome, #{new_name}. You are number 1 in line."
+    else
+        person.push(new_name)
+        puts "Welcome, #{new_name}. You are number #{person.length} in line."
+    
+
+     
+    end
+end
+
+def now_serving(plce)
+    
+    if plce.length == 0
+        puts "There is nobody waiting to be served!"
+    else
+        puts "Currently serving #{plce.shift}."
+         
+    end 
+end
